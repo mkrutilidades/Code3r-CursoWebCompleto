58e84b27e3de6a7e514239691e844222d1e6e86d
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-02-10 17:43:56 -0300

    js - Atriuição

---
+++ WebModerno/EXERCICIOS-JS/fundamentos/atribuicao.js
@@ -0,0 +1,22 @@
+const a = 7
+let b = 3
+
+b += a // b = b + a
+console.log(a, b)
+
+b -= 4 // b = b - 4
+console.log(a, b)
+
+b *= 2 // b = b * 2
+console.log(a, b)
+
+b /= 2 // b = b * 2
+console.log(a, b)
+
+b *= 2 // b = b / 2
+console.log(a, b)
+
+b %= 2 // b = b % 2
+console.log(a, b) // Resto da divisão (Par = 0 e Impar = 1)
+
+
