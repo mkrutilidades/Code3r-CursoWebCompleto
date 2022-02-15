1006bcba710ba08402a4861033653af45dbd0e3f
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-02-15 14:57:40 -0300

    fundamentos - Revendo Destructuring

--- WebModerno/EXERCICIOS-JS/fundamentos/destructuring3.js
+++ WebModerno/EXERCICIOS-JS/fundamentos/destructuring3.js
@@ -5,6 +5,9 @@ function rand({ min = 0, max = 1000}) {
 
 const obj = { max: 50, min: 40}
 console.log(rand(obj))
+
 console.log(rand({min: 955}))
+
 console.log(rand({}))
+
 //console.log(rand()) //- Neste caso vai dar problema
