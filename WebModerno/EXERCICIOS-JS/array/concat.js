ed062a6d61fa0a0e87f381dfd682fd565b9c6b6b
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-03-11 18:33:00 -0300

    Final array

---
+++ WebModerno/EXERCICIOS-JS/array/concat.js
@@ -0,0 +1,14 @@
+const filhas = [ 'Valeskah', 'Cibalena' ]
+const filhos = [ 'Uoxiton', 'Uesclei' ]
+const todos = filhas.concat(filhos)
+
+// console.log(filhas)
+// console.log(filhos)
+// console.log(todos)
+
+//Concatenando arrays e adicionando elemento ao mesmo tempo
+// const todosMaisUm = filhas.concat(filhos, 'Amante')
+// console.log(todos)
+// console.log(todosMaisUm)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6,7]]))
